095eb23fa8d323e3a191de5d55b028bd34d2c31a
Dylan <142943600+dbrun3@users.noreply.github.com>
2024-05-17 18:59:11 -0500

    Update index.ts

--- Bubble/src/router/index.ts
+++ Bubble/src/router/index.ts
@@ -2,20 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 // @ts-ignore
 import Login from '@/pages/Login.vue'
 // @ts-ignore
-import Main from '@/pages/Main.vue'
+import Map from '@/pages/Map.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'Login',
       component: Login
     },
     {
-      path: '/main',
-      name: 'Main',
-      component: Main
+      path: '/map',
+      name: 'Map',
+      component: Map
 
 
       // route level code-splitting
